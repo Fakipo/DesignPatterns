f2fc80f3e15e3b59ae247a3b0c19fad3b454463f
Fakipo <paraskavdikar@gmail.com>
2025-02-20 15:55:56 -0700

    Added folder structure for abstract factory and added readme file

---
+++ src/designpatterns/abstractfactory/Application.java
@@ -0,0 +1,7 @@
+package designpatterns.abstractfactory;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Welcome to abstract factory design pattern");
+    }
+}
