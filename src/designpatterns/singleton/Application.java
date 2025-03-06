181b43df325a9724437bff705940cc657fcfd33d
Fakipo <paraskavdikar@gmail.com>
2025-03-06 03:18:08 -0700

    Started Code for singleton pattern

---
+++ src/designpatterns/singleton/Application.java
@@ -0,0 +1,8 @@
+package designpatterns.singleton;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("Hello world how are you actually doing?");
+
+    }
+}
