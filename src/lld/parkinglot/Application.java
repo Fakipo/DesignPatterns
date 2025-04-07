4abeecf13413cf1908a5415dc87821ed0b9b7c8d
Fakipo <paraskavdikar@gmail.com>
2025-04-07 11:22:47 -0700

    Added the code for Pizza price calculation LLD

--- src/lld/parkinglot/Application.java
+++ src/lld/parkinglot/Application.java
@@ -1,4 +1,4 @@
-package designpatterns.LLD.parkinglot;
+package lld.parkinglot;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/lld/pizzapricecalculation/Application.java
@@ -0,0 +1,7 @@
+package lld.pizzapricecalculation;
+
+public class Application {
+    public static void main(String [] args){
+        System.out.println("Hello we are in pizza calculation");
+    }
+}
